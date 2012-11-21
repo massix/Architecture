6ce05ce43a30fd98c1a1f67251636fedff3a6f18
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2012-11-21 16:16:55 +0100

    Begin backend interface implementation

---
+++ include/BackendItf.h
@@ -0,0 +1,19 @@
+/*
+ * BackendItf.h
+ *
+ *  Created on: 21/nov/2012
+ *      Author: massi
+ */
+
+#pragma once
+
+
+class BackendItf
+{
+public:
+	BackendItf();
+	virtual ~BackendItf();
+
+private:
+protected:
+};
---
+++ src/BackendItf.cpp
@@ -0,0 +1,10 @@
+/*
+ * BackendItf.cpp
+ *
+ *  Created on: 21/nov/2012
+ *      Author: massi
+ */
+
+
+
+
